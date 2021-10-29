5d812e3593cbcb107929571453ddfcd00a63d3ea
wahidurrahman85 <ebiz.wahid@gmail.com>
2021-10-29 16:36:26 +0600

    first commit

---
+++ src/firebase/Firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAcV3Z7s9KeVcQMXKkA4_ImBJVf0rhXI3k",
+  authDomain: "fir-authentication-apply.firebaseapp.com",
+  projectId: "fir-authentication-apply",
+  storageBucket: "fir-authentication-apply.appspot.com",
+  messagingSenderId: "797051857669",
+  appId: "1:797051857669:web:cab8bff0f96a0229ce256c",
+};
+
+export default firebaseConfig;
